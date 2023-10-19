f45be9d5ecf2def2baf3798047563f4ce95d7484
mdshakil04 <m.shakil04@yahoo.com>
2023-10-19 22:42:12 +0600

    Ratting added to the new Products

--- src/Pages/NewProducts/NewProducts.jsx
+++ src/Pages/NewProducts/NewProducts.jsx
@@ -1,11 +1,12 @@
 const NewProducts = () => {
   return (
     <div className=" border p-8">
-        <h2 className="text-4xl text-center my-8">Our New Products</h2>
+      <h2 className="text-4xl text-center my-8">Our New Products</h2>
       <div className=" container mx-auto lg:grid lg:grid-cols-3 mb-4 gap-4">
         <div className="card bg-base-100 shadow-xl">
           <figure>
-            <img className=" h-[350px]"
+            <img
+              className=" h-[350px]"
               src="https://i.ibb.co/0CWw4Nk/Oppo-Reno-8-Pro-cameras.webp"
               alt="phone"
             />
@@ -15,6 +16,36 @@ const NewProducts = () => {
               Oppo-Reno-8-Pro-cameras
               <div className="badge badge-secondary">NEW</div>
             </h2>
+            <div>
+              <div className="rating">
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                />
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                  checked
+                />
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                />
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                />
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                />
+              </div>
+            </div>
             <div className="card-actions justify-end">
               <div className="badge badge-outline">Smart Phone</div>
               <div className="badge badge-outline">Products</div>
@@ -23,16 +54,47 @@ const NewProducts = () => {
         </div>
         <div className="card bg-base-100 shadow-xl">
           <figure>
-            <img className=" h-[350px]"
+            <img
+              className=" h-[350px]"
               src="https://i.ibb.co/mGGQGBq/OPPO-A78-5-G-9.jpg"
               alt="phone"
             />
           </figure>
           <div className="card-body">
             <h2 className="card-title">
-            OPPO-A78-5-G-9
+              OPPO-A78-5-G-9
               <div className="badge badge-secondary">NEW</div>
             </h2>
+            <div>
+              <div className="rating">
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                />
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                  checked
+                />
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                />
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                />
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                />
+              </div>
+            </div>
             <div className="card-actions justify-end">
               <div className="badge badge-outline">Smart Phone</div>
               <div className="badge badge-outline">Products</div>
@@ -41,16 +103,47 @@ const NewProducts = () => {
         </div>
         <div className="card bg-base-100 shadow-xl">
           <figure>
-            <img className=" min-w-full h-[350px]"
+            <img
+              className=" min-w-full h-[350px]"
               src="https://i.ibb.co/BLPGx9W/oppo-f17-pro-featured.webp"
               alt="phone"
             />
           </figure>
           <div className="card-body">
             <h2 className="card-title">
-            Oppo-f17-pro
+              Oppo-f17-pro
               <div className="badge badge-secondary">NEW</div>
             </h2>
+            <div>
+              <div className="rating">
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                />
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                  checked
+                />
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                />
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                />
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                />
+              </div>
+            </div>
             <div className="card-actions justify-end">
               <div className="badge badge-outline">Smart Phone</div>
               <div className="badge badge-outline">Products</div>
